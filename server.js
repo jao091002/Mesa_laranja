c53b28bf097b991fe7295913299617679d6b2b6a
jvzin2k09 <joaovictorramoscoelho143@gmail.com>
2026-03-02 11:05:24 -0300

    atualizando rota

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir arquivos estáticos
+
 app.use(express.static(path.join(__dirname)));
 
-// Rota principal
-app.get("/", (req, res) => {
+app.get("/schumacher", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Iniciar servidor
+
 app.listen(PORT, () => {
   console.log(`🔥 Servidor rodando em http://localhost:${PORT}`);
 });
